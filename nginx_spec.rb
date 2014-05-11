a8b64917af277cbf4098502d74bdaacd17994c59
Aleksei Feltin <aleksf@maia.local>
2014-05-11 13:26:12 +1000

    Extended Vagrantfile with vagrant-serverspec
    Added nginx serverspec test to test for listening port, process,
    service, package

---
+++ nginx_spec.rb
@@ -0,0 +1,18 @@
+require_relative 'spec_helper'
+
+describe command('pgrep nginx') do
+  it { should return_exit_status 0 }
+end
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe port(8000) do
+  it { should be_listening }
+end
